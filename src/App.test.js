7f9712f3a758ead1366fc01ea16cba45b4658506
marie-hotchkiss <mtknowlton67@gmail.com>
2020-06-03 23:10:48 -0700

    added mock data

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,35 @@
 import react from "react";
+import {render, fireevent, waitFor, act} from "@testing-library/react";
+import { fetchShow as  mockFetchShow } from "./api/fetchShow";
+
+import App from "./App";
+
+jest.mock("./api/fetchShow");
+
+test("App fetches show data and renders it", async () => {
+    const mockData = {
+        image: { original: "original"},
+        name: "name",
+        summary: "<p>summary</p>",
+        _embedded: {
+          episodes: [{
+              id: "123",
+              image: { medium: "medium_image" },
+              name: "name",
+              season: 3,
+              number: 2,
+              summary: "<p>Summary</p>",
+              runtime: 20
+          }]
+        }
+    };
+    mockFetchShow.mockResolvedValueOnce(mockData);
+    const { queryAllByText, debug, getByText } = render(<App />);
+  //   debug();
+    expect(queryAllByText(/fetching data.../i)).toHaveLength(1);
+    await waitFor(() => {
+      expect(queryAllByText(/summary/i)).toHaveLength(1);
+      });
+  
+  });
+  
